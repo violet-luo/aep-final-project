a115e30be81721c9728763137b9f5de488b0247e
Violet Luo <luo.violeta@gmail.com>
2023-04-16 13:59:35 -0700

    Update first test

--- test_mahjong.py
+++ test_mahjong.py
@@ -5,7 +5,7 @@ class TestMahjong(unittest.TestCase):
     # 1. Test if Mahjong has exactly four players
     def test_four_players_should_return_true(self):
         mahjong = Mahjong([[],[],[],[]])
-        self.assertTrue(mahjong.has_4_players)
+        self.assertTrue(mahjong.has_4_players())
 
     def test_three_players_should_return_false(self):
         mahjong = Mahjong([[],[],[]])
