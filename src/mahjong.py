060c09a2c80ab79720b62a7076535243d3b712d8
Violet Luo <luo.violeta@gmail.com>
2023-04-16 14:03:30 -0700

    Add 2nd test

--- src/mahjong.py
+++ src/mahjong.py
@@ -5,3 +5,10 @@ class Mahjong:
     # 1. Mahjong has exactly four players.
     def has_4_players(self):
         return len(self.tiles) == 4
+    
+    # 2. Each player has 14 tiles.
+    def each_player_has_14_tiles(self):
+        for tile in self.tiles:
+            if len(tile) != 14:
+                return False
+        return True
--- test_mahjong.py
+++ test_mahjong.py
@@ -11,5 +11,14 @@ class TestMahjong(unittest.TestCase):
         mahjong = Mahjong([[],[],[]])
         self.assertFalse(mahjong.has_4_players())
 
+    # 2. Test if each player has 14 tiles.
+    def test_each_player_has_14_tiles_should_return_true(self):
+        mahjong = Mahjong([[1]*14, [2]*14, [3]*14, [4]*14])
+        self.assertTrue(mahjong.each_player_has_14_tiles())
+
+    def test_each_player_has_13_tiles_should_return_false(self):
+        mahjong = Mahjong([[1]*14, [2]*14, [3]*14, [4]*13])
+        self.assertFalse(mahjong.each_player_has_14_tiles())
+
 if __name__ == '__main__':
     unittest.main()
